f7bac7ae859694c9aeb75a220cae64dd644edaa5
dennis-hh <dennis.kopp84@googlemail.com>
2016-04-03 19:54:47 +0200

    preps for 0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 
-## master
+## 0.0.3
+- many changes under the hood
+- added recovery of cached accessories on restart
+
+## 0.0.2
 - fix for getting device state not working properly
 
 ## 0.0.1 (2016-03-23)
--- index.js
+++ index.js
@@ -1,41 +1,128 @@
 var RaumfeldManager = require("node-raumfeld");
 var loglevel = require("loglevel");
-var RendererAccessory = require("./lib/RaumfeldRendererAccessory");
-var utils = require("./lib/utils");
+var VolumeCharacteristic = require("./lib/VolumeCharacteristic");
 //loglevel.setLevel("debug");
 
 module.exports = function(homebridge) {
-    // Accessory must be created from PlatformAccessory Constructor
-    Accessory = homebridge.hap.Accessory;
+    Accessory = homebridge.platformAccessory;
+    UUIDGen = homebridge.hap.uuid;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
     Homebridge = homebridge;
 
-    utils.addSupportTo(RendererAccessory, Accessory);
-    homebridge.registerPlatform("homebridge-raumfeld", "Raumfeld", RaumfeldPlatform);
+    homebridge.registerPlatform("homebridge-raumfeld", "Raumfeld", RaumfeldPlatform, true);
 }
 
 function RaumfeldPlatform(log, config, api) {
     this.log = log;
+    this.api = api;
     this.config = config;
+    this.accessories = [];
     this.manager = new RaumfeldManager();
 
     var self = this;
-    this.manager.discover(true);
+
+    this.api.on('didFinishLaunching', function() {
+    	self.log("Plugin finished launching");
+        self.manager.discover("virtual");
+        self.manager.on("rendererFound", function(renderer) {
+            self.registerAccessory(renderer);
+        });
+  	}.bind(this));
 }
 
-RaumfeldPlatform.prototype.accessories = function(callback) {
+RaumfeldPlatform.prototype.configureAccessory = function(accessory) {
     var self = this;
-    setTimeout(function() {
-        callback(self.createAccessories());
-    }, 5000);
+    this.log("Plugin - Configure Accessory: " + accessory.displayName);
+    var rendererUrl = accessory.context.rendererUrl;
+    accessory.reachable = false;
+    self.accessories[accessory.UUID] = accessory;
+    this.manager.createDevice(rendererUrl).then(function(device) {
+        self.addSwitchService(accessory, device);
+        accessory.reachable = true;
+    });
 }
 
-RaumfeldPlatform.prototype.createAccessories = function() {
-  var self = this;
-  var accessories = [];
+RaumfeldPlatform.prototype.configurationRequestHandler = function(context, request, callback) {
+    this.log("Context: ", JSON.stringify(context));
+    this.log("Request: ", JSON.stringify(request));
+}
 
-  this.manager.getRenderers().forEach(function(renderer) {
-    accessories.push(new RendererAccessory(self, Homebridge, renderer));
-  });
+RaumfeldPlatform.prototype.registerAccessory = function(renderer) {
+    var accessory = this.createAccessory(renderer);
+      if(accessory.UUID in this.accessories) {
+          if(accessory.displayName === this.accessories[accessory.UUID].displayName) {
+              this.log(accessory.displayName + " already registered; skipping");
+              return;
+          }
+          this.log("unregistering " + accessory.displayName);
+          this.api.unregisterPlatformAccessories("homebridge-raumfeld", "Raumfeld", [this.accessories[accessory.UUID]]);
+          //wait one cycle
+          return;
+      }
+      accessory.reachable = true;
+      this.accessories[accessory.UUID] = accessory;
+      this.log("registering " + accessory.displayName);
+      this.api.registerPlatformAccessories("homebridge-raumfeld", "Raumfeld", [accessory]);
+}
 
-  return accessories;
+RaumfeldPlatform.prototype.createAccessory = function(renderer) {
+    var uuid = Homebridge.hap.uuid.generate(String(renderer.uuid));
+    var accessory = new Accessory(renderer.name, uuid);
+    accessory.context.rendererUrl = renderer.client.url;
+    var informationService = accessory.getService(Homebridge.hap.Service.AccessoryInformation);
+
+    informationService
+        .setCharacteristic(Characteristic.Manufacturer, renderer.manufacturer)
+        .setCharacteristic(Characteristic.Model, renderer.modelNumber);
+        //.setCharacteristic(this.homebridge.hap.Characteristic.SerialNumber, this.renderer.serialNumber)
+
+    this.addSwitchService(accessory, renderer);
+
+    return accessory;
+}
+
+RaumfeldPlatform.prototype.addSwitchService = function(accessory, renderer) {
+    accessory.on("identify", function(paired, callback) {
+        console.log("identify " + accessory.displayName + " - is paired: " + paired);
+        callback();
+    })
+    var switchService = accessory.getService(Homebridge.hap.Service.Switch);
+    if (switchService != undefined) {
+        //seems to be an accessory recovered from cache; we have to recreate switch service
+        accessory.removeService(switchService);
+    }
+    switchService = new Homebridge.hap.Service.Switch(accessory.displayName);
+    switchService
+        .getCharacteristic(Characteristic.On)
+        .on('get', function(callback) {
+            renderer.getState().then(function(state) {
+                callback(null, state);
+            })
+        })
+        .on('set', function(on, callback) {
+            if(on) {
+                renderer.play().then(function(result) {
+                    callback(null);
+                });
+            } else {
+                renderer.stop().then(function(result) {
+                    callback(null);
+                });
+            }
+        });
+        switchService
+            .addCharacteristic(VolumeCharacteristic)
+            .on('get', function(callback) {
+                renderer.getVolume().then(function(volume) {
+                    callback(null, Number(volume));
+                })
+            })
+            .on('set', function(volume, callback) {
+                renderer.setVolume(volume).then(function() {
+                    callback(null);
+                })
+            });
+
+    accessory.addService(switchService);
 }
